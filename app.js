d7666621fb516cdd7bf23cfbb4a1115abac8cb6f
imike964 <imike964@googlemail.com>
2023-01-28 13:48:28 +0000

    starting JS

---
+++ app.js
@@ -0,0 +1,20 @@
+// // document.querySelectorAll("#rate-button").addEventListener("click", setChoice);
+// document.getElementById("rate-button-1").addEventListener("click", setChoice);
+// document.getElementById("rate-button-2").addEventListener("click", setChoice);
+// document.getElementById("rate-button-3").addEventListener("click", setChoice);
+// document.getElementById("rate-button-4").addEventListener("click", setChoice);
+// document.getElementById("rate-button-5").addEventListener("click", setChoice);
+
+
+
+
+// function setChoice(){
+
+
+//     console.log("button pressed");
+
+//     document.getElementsByClassName("button-container").style.backgroundColor = "#004f40";
+//     this.style.backgroundColor = "#004f40";  
+
+
+// }
