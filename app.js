a00e6d463530236c9bbab1a9a43e4b7ac3dc734d
imike964 <imike964@googlemail.com>
2023-01-31 14:49:13 +0000

    The select button works now, thanks Khalid

--- app.js
+++ app.js
@@ -5,16 +5,32 @@
 // document.getElementById("rate-button-4").addEventListener("click", setChoice);
 // document.getElementById("rate-button-5").addEventListener("click", setChoice);
 
+// const buttons = document.querySelectorAll(".button-container button");
 
-
-
-// function setChoice(){
-
-
-//     console.log("button pressed");
-
-//     document.getElementsByClassName("button-container").style.backgroundColor = "#004f40";
-//     this.style.backgroundColor = "#004f40";  
-
-
+// let activeButton = null;
+// for (let button of buttons) {
+//   button.addEventListener("click", function () {
+//     if (activeButton) {
+//       activeButton.classList.remove("active-button");
+//     }
+//     activeButton.classList.add("active-button");
+//     activeButton = this;
+//     console.log(this);
+//   });
 // }
+
+const buttons = document.querySelectorAll(".button-container button");
+console.log(buttons);
+let activeButton = null;
+
+for (let button of buttons) {
+  button.addEventListener("click", function (e) {
+    if (activeButton) {
+      activeButton.classList.remove("active-button");
+      activeButton.classList.add("button");
+    }
+    activeButton = this;
+    activeButton.classList.add("active-button");
+    activeButton.classList.remove("button");
+  });
+}
